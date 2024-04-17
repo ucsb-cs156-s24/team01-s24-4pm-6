803e04ae6810d76a6cf190ed4d0cffa4608ee99f
Dakota Barnes <dakotabarnes@169-231-39-30.wireless.ucsb.edu>
2024-04-17 16:17:02 -0700

    Dakota - PublicHoliday add tests and update Service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
@@ -27,7 +27,7 @@ public class PublicHolidayQueryService {
         restTemplate = restTemplateBuilder.build();
     }
 
-    public static final String ENDPOINT = "\thttps://date.nager.at/api/v2/publicholidays/{year}/{countryCode}";
+    public static final String ENDPOINT = "https://date.nager.at/api/v2/publicholidays/{year}/{countryCode}";
 
     public String getJSON(String year, String countryCode) throws HttpClientErrorException {
         log.info("year={}, countryCode={}", year, countryCode);
