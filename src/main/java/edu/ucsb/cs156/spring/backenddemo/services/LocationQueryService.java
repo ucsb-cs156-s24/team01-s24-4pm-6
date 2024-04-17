4b78521131ff1eddf45f3a05fb8c3d141fb2786b
nbelle1 <89555340+nbelle1@users.noreply.github.com>
2024-04-16 17:42:46 -0700

    NB - define ENDPOINT for location service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -1,11 +1,9 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
-
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-
 
 @Service
 public class LocationQueryService {
@@ -16,7 +14,7 @@ public class LocationQueryService {
         restTemplate = restTemplateBuilder.build();
     }
 
-    public static final String ENDPOINT = "";
+    public static final String ENDPOINT = "https://nominatim.openstreetmap.org/search/search.php?q={location}&format=jsonv2";
 
     public String getJSON(String location) throws HttpClientErrorException {
         return "";
